731eac88aca130a1751d3cb6648465be3b7d4392
t7r0n <55046689+t7r0n@users.noreply.github.com>
2022-04-21 23:00:37 +0530

    Add files via upload

---
+++ fire3.bat
@@ -0,0 +1,34 @@
+@echo off
+
+:: Bloomberg Quicktake: Originals
+start firefox -private-window https://www.youtube.com/BloombergTV/videos
+
+:: MagnatesMedia
+start firefox -private-window https://www.youtube.com/c/MagnatesMedia/videos
+
+:: Vox
+start firefox -private-window https://www.youtube.com/c/Vox/videos
+
+:: Wall Street Journal  
+start firefox -private-window https://www.youtube.com/c/wsj/videos
+
+:: neo
+start firefox -private-window https://www.youtube.com/c/neoyoutube/videos
+
+:: Real Engineering
+start firefox -private-window https://www.youtube.com/c/RealEngineering/videos
+
+:: PolyMatter
+start firefox -private-window https://www.youtube.com/c/PolyMatter/videos
+
+:: Wendover Productions
+start firefox -private-window https://www.youtube.com/c/Wendoverproductions/videos
+
+:: Wired
+start firefox -private-window https://www.youtube.com/c/wireduk/videos
+
+:: Two Minute Papers
+start firefox -private-window https://www.youtube.com/c/K%C3%A1rolyZsolnai/videos
+
+:: Motherboard
+start firefox -private-window https://www.youtube.com/motherboard/videos
